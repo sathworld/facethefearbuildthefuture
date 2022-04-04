1cfcd41802d580a2964aff2f3cf25a73120d7f4e
sathworld <48067596+sathworld@users.noreply.github.com>
2022-04-03 20:05:55 -0400

    Add files via upload

---
+++ LC.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         LC Logo template
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the canvas!
+// @author       oralekin + odgrim
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://raw.githubusercontent.com/sathworld/facethefearbuildthefuture/main/FACETHEFEAR.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
